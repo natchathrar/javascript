08e5eb78a882d90d5b1a4aa7ab7b590e6e024ead
Natchathra <121845723+natchathrar@users.noreply.github.com>
2023-03-02 14:00:40 +0530

    Delete startend.js

--- startend.js
+++
@@ -1,5 +0,0 @@
-const person = ['Nandy', 'Sweden', 'Skinny' , 'Candy', 'Sandy','Natchathra' ,'Abishek','Samy',  ];
-
-const out = person.filter((letter) => letter.startsWith("S") && letter.endsWith("y"));
-
-console.log(out);
