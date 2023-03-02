dc393317f3c715059842d50622688e50f8b667d5
Natchathra <121845723+natchathrar@users.noreply.github.com>
2023-03-02 13:58:22 +0530

    Delete form.js

--- form.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('btn1').onclick = function(){
- var firstname = document.getElementById('fname').value;
- var lastname = document.getElementById('lname').value;
- document.getElementById('result').innerHTML=firstname + " "+ lastname;
-}
-
-
