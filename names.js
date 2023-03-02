cb82ac936c25b4edd73b974f6e6f2cd2121d5723
Natchathra <121845723+natchathrar@users.noreply.github.com>
2023-03-02 14:02:04 +0530

    Delete names.js

--- names.js
+++
@@ -1,7 +0,0 @@
-const names =["Amirtha","Sindhu","Natchathra","Saranya","Asha","Nisha","Anushuya",];
-
-var filteredNames = names.filter(function(word) {
-    return word[0] === "A";
-});
-
-console.log(filteredNames);
